f106c3493bbfee6dca04d12756deb93596bf9c44
Enzo Harquin <enzo.harquin@etu.utc.fr>
2022-10-04 15:54:48 +0200

    Ajoutez des fichiers projet.

---
+++ src/app.cpp
@@ -0,0 +1,9 @@
+#include "app.hpp"
+
+void App::run()
+{
+	while (!window.shouldClose())
+	{
+		glfwPollEvents();
+	}
+}
---
+++ src/window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+// libs
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+// std
+#include <string>
+
+class Window
+{
+public:
+	Window(int w, int h, std::string name);
+	~Window();
+
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+
+	bool shouldClose() { return glfwWindowShouldClose(window); }
+
+private:
+	void initWindow();
+
+	const int width;
+	const int height;
+
+	std::string windowName;
+	GLFWwindow* window;
+};
+
